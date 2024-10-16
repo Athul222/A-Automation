cb2c1443808d96266818d95d4ec50f3aca7a3a0d
ATHUL <116086492+Athul222@users.noreply.github.com>
2024-10-16 08:53:22 +0530

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -1,5 +1,4 @@
 from time import sleep
-from dotenv import load_dotenv, dotenv_values
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
